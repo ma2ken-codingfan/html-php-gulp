ff2ff98d28558922518da7f3c46f28c14909936e
Codingfan <ma2ken.codingfan@gmail.com>
2024-01-28 16:12:14 +0900

    Translate explanation to English

---
+++ READ.md
@@ -0,0 +1,13 @@
+# html file in php
+
+## cmd
+
+``` bash
+npm install
+
+gulp
+```
+
+## If browser-sync doesn't reload
+
+If there is no body tag in the html file, it will not be reloaded
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const browser = require("browser-sync");
 const connectPhp = require( "gulp-connect-php" );
 
-//オートリロードのタスク
+//Auto Reload
 gulp.task('server', ()=> {
   connectPhp.server({
         livereload: true,
@@ -10,34 +10,34 @@ gulp.task('server', ()=> {
     base:'./src/'
   }, function (){
         browser.init({
-      proxy: 'http://mysql.host:80',
+      proxy: 'http://crud.host:80', // local domain
     });
   });
 });
 
-//HTMLのタスク
+//HTML task
 gulp.task('html', ()=> {
-  return gulp.src(['./app/**/*.html'])
+  return gulp.src(['./src/**/*.html'])
   .pipe(browser.reload({stream:true}))
 });
 
-//phpのタスク
+//php task
 gulp.task('php', ()=> {
-  return gulp.src(['./app/**/*.php'])
+  return gulp.src(['./src/**/*.php'])
   .pipe(browser.reload({stream:true}))
 });
 
-//jsのタスク
+//js task
 gulp.task('js', ()=> {
-  return gulp.src(['./app/**/*.js'])
+  return gulp.src(['./src/**/*.js'])
   .pipe(browser.reload({stream:true}))
 });
 
-// 監視
-gulp.task( 'watch', function(done) {
-  gulp.watch( './app/**/*.html', gulp.task('html') );
-  gulp.watch( './app/**/*.php', gulp.task('php') );
-  gulp.watch( './app/**/*.js', gulp.task('js') );
+// watch
+gulp.task('watch', function(done) {
+  gulp.watch( './src/**/*.html', gulp.task('html') );
+  gulp.watch( './src/**/*.php', gulp.task('php') );
+  gulp.watch( './src/**/*.js', gulp.task('js') );
 });
 
 // default
